0e4c0e2d937568dd01ad1160dd600ccb8c20ffef
sband1 <sbandeali1@gmail.com>
2020-10-20 21:18:07 -0700

    Refactor code to implement search query

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,76 @@ import VideoList from './VideoList.js';
 import VideoPlayer from './VideoPlayer.js';
 
 class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       //placeholder videolists for event handler
-      allVideos: exampleVideoData,
-      currentVideo: exampleVideoData[0],
+      allVideos: [{
+        kind: 'youtube#searchResult',
+        etag: 'abQHWywil_AkNqdqji7_FqiK-u4/Ykxo_CqKu8F8kcm-iNgL332gQTY',
+        id: {
+          kind: 'youtube#video',
+          videoId: '4ZAEBxGipoA'
+        },
+        snippet: {
+          publishedAt: '2015-08-02T20:52:58.000Z',
+          channelId: 'UCJbPGzawDH1njbqV-D5HqKw',
+          title: 'React JS Tutorial for Beginners - 1 - Introduction',
+          description: 'My website - https://www.thenewboston.com/videos.php Have questions about the tutorial or React? Ask them here ...',
+          thumbnails: {
+            default: {
+              url: 'https://i.ytimg.com/vi/4ZAEBxGipoA/default.jpg',
+              width: 120,
+              height: 90
+            },
+            medium: {
+              url: 'https://i.ytimg.com/vi/4ZAEBxGipoA/mqdefault.jpg',
+              width: 320,
+              height: 180
+            },
+            high: {
+              url: 'https://i.ytimg.com/vi/4ZAEBxGipoA/hqdefault.jpg',
+              width: 480,
+              height: 360
+            }
+          },
+          channelTitle: 'thenewboston',
+          liveBroadcastContent: 'none'
+        }
+      }],
+      currentVideo: {
+        kind: 'youtube#searchResult',
+        etag: 'abQHWywil_AkNqdqji7_FqiK-u4/Ykxo_CqKu8F8kcm-iNgL332gQTY',
+        id: {
+          kind: 'youtube#video',
+          videoId: '4ZAEBxGipoA'
+        },
+        snippet: {
+          publishedAt: '2015-08-02T20:52:58.000Z',
+          channelId: 'UCJbPGzawDH1njbqV-D5HqKw',
+          title: 'React JS Tutorial for Beginners - 1 - Introduction',
+          description: 'My website - https://www.thenewboston.com/videos.php Have questions about the tutorial or React? Ask them here ...',
+          thumbnails: {
+            default: {
+              url: 'https://i.ytimg.com/vi/4ZAEBxGipoA/default.jpg',
+              width: 120,
+              height: 90
+            },
+            medium: {
+              url: 'https://i.ytimg.com/vi/4ZAEBxGipoA/mqdefault.jpg',
+              width: 320,
+              height: 180
+            },
+            high: {
+              url: 'https://i.ytimg.com/vi/4ZAEBxGipoA/hqdefault.jpg',
+              width: 480,
+              height: 360
+            }
+          },
+          channelTitle: 'thenewboston',
+          liveBroadcastContent: 'none'
+        }
+      },
       done: false
     };
   }
@@ -21,6 +85,31 @@ class App extends React.Component {
       // grab index to selected video in exampleVideoData array
       currentVideo: vid
     });
+  }
+
+
+  // make a function that gets you tube videos
+  // Search constructor provides params details
+  getYouTubeVideos(query) {
+    // takes in searchVid object and calls youtubeSearch in searchYouTube.js
+    var options = {
+      key: this.props.APIKey,
+      query: query,
+    };
+    //console.log('yo', this.props);
+    this.props.searchYouTube(options, (data) => {
+      this.setState({
+        allVideos: data.items,
+        currentVideo: this.state.allVideos[0]
+      });
+    });
+    // youtubeSearch runs ajax GET request
+  }
+
+  componentDidMount() {
+    // Give search details into getYouTubeVideos fn
+    console.log('component mount invocation');
+    this.getYouTubeVideos('cats');
   }
 
   render() {
